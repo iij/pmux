f24239f3e373171463aa0e010866fcd549f47db8
maebashi <maebashi@iij.ad.jp>
2012-12-25 13:17:06 +0900

    accept multiple glob patterns

--- lib/pmux/cleaner.rb
+++ lib/pmux/cleaner.rb
@@ -2,18 +2,18 @@ require 'fileutils'
 
 module Pmux
   class Cleaner
-    def initialize glob_pat
-      @glob_pat = glob_pat
+    def initialize *glob_pats
+      @glob_pats = glob_pats
     end
 
     def run lim_time=nil
-      fork {fork {clean @glob_pat, lim_time}}
+      fork {fork {@glob_pats.each {|glob_pat| clean glob_pat, lim_time}}}
       Process.wait
     end
 
     def clean glob_pat=nil, lim_time=nil
       glob_pat ||= @glob_pat
-      lim_time ||= Time.now - 3600*24
+      lim_time ||= Time.now - 3600*24*7
       paths = Dir.glob glob_pat
       for path in paths
         if File.exist? path
